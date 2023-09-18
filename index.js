d91623659a5a1c7dd75958fdc7d105d922e65229
lekkha <lekhasoni001@gmail.com>
2023-09-18 18:58:21 +0530

    final changes

--- index.js
+++ index.js
@@ -3,27 +3,63 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
-//all the static files are in html
-const static = express.static("public"); 
+var admin = require("firebase-admin");
+var serviceAccount = require("./shorten-f103b-firebase-adminsdk-if7ln-5c317078f9.json");
+const { response } = require("express");
 
-//static middleware able to put up files 
-app.use(static); 
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const static = express.static("public");
+
+const urlsdb = admin.firestore().collection("urlsdb");
+const usersdb = admin.firestore().collection("usersdb");
+
+app.use(static);
 app.use(bodyParser.json());
 
-//creating a middleware 
-// app.use((req, res, next) => { 
-//   console.log("we intercepted the request");
-//   next; 
+// app.use((req, res, next) => {
+//     console.log("We intercepted the req");
+//     next();
 // })
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
+app.get("/:short", (req, res) => {
+    console.log(req.params);
+    const short = req.params.short;
 
-//what is post operation what is get operation 
+    const doc = urlsdb.doc(short);
+
+    doc.get().then(response => {
+        const data = response.data();
+        // console.log(data);
+        if(data && data.url){
+            res.redirect(301, data.url);
+        } else {
+            res.redirect(301, "https://codeforcause.org");
+        }
+    })
+
+    // res.send("We will redirect you to " + short)
+});
+
 app.post("/admin/urls/", (req, res) => {
-  console.log(req.body); 
-  res.send("Hello from another!");
+    const {email, password, short, url} = req.body;
+
+    usersdb.doc(email).get().then(response=>{
+        const user = response.data();
+        // console.log(user);
+
+        if(user && (user.email == email) && (user.password == password)){
+            const doc = urlsdb.doc(short);
+            doc.set({url});
+            res.send("Done")
+        } else {
+            res.send(403, "Not possible")
+        }
+    })
+  
+//   res.send("Hello from another!");
 });
 
 app.listen(port, () => {
